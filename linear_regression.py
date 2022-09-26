ba311b6e9216debcd42cad1d6abe1cf35d53feee
Tedington <tdansa64@gmail.com>
2022-09-27 00:00:09 +0100

    converted the result to a list allowing to show the index value of selected index

--- linear_regression.py
+++ linear_regression.py
@@ -36,7 +36,7 @@ for feature_name in NUMERIC_COLUMNS:
 # Creating an input function that convert the current pandas df into a tf.data.Dataset object
 def make_input_fn ( data_df, label_df, num_epochs=10, shuffle=True, batch_size=32):
     def input_function(): # inner function, this will be returned
-        ds = tf.data.Dataset.from_tensor_slices(dict(data_df), label_df) 
+        ds = tf.data.Dataset.from_tensor_slices((dict(data_df), label_df)) 
         if shuffle:
             ds = ds.shuffle(1000) # randomize order of data
         ds = ds.batch(batch_size).repeat(num_epochs) # split dataset into batches of 32 and repeat process for number of epochs
@@ -44,7 +44,7 @@ def make_input_fn ( data_df, label_df, num_epochs=10, shuffle=True, batch_size=3
     return input_function # return a function object for use
 
 train_input_fn = make_input_fn(dftrain, y_train) # we call the input_function that was returned to us to get a dataset object we can feed to the model
-eval_input_fn = make_input_fn(dfeval, y_eval, epoch = 1, shuffle=False)
+eval_input_fn = make_input_fn(dfeval, y_eval, num_epochs=1, shuffle=False)
 
 # Create a linear estimator by passing the feature columns we created earlier 
 linear_est = tf.estimator.LinearClassifier(feature_columns=feature_columns)
@@ -54,7 +54,10 @@ linear_est.train(train_input_fn) # train
 result = linear_est.evaluate(eval_input_fn) # get model metrics/stats by testing on testing data
 
 clear_output()
-print(result['accuracy']) # the result variable is simply a dict of stats about our model
+print(result) # the result variable is simply a dict of stats about our model
 
+result = list(linear_est.predict(eval_input_fn))
+print(result)
+print(dfeval.loc[0])
+print(result[0]['probabilities'] [0]) #shows the value according to the index
 
-print(feature_columns) 
