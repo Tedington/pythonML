97fe8b86bf2e1392f02af706f912eb518c436812
Tedington <tdansa64@gmail.com>
2022-09-27 19:42:55 +0100

    A classification model written in python

---
+++ classification_model.py
@@ -0,0 +1,39 @@
+from __future__ import absolute_import, division, print_function, unicode_literals
+from termios import CS5
+
+import tensorflow as tf
+import pandas as pd
+
+CSV_COLUMN_NAMES = ['SepalLength', 'SepalWidth', 'PetalLength', 'PetalWidth', 'Species']
+SPECIES = ['Setosa', 'Versicolor', 'Virginica']
+
+# Here we use keras (a module inside of tensorflow) to grab our datasets and read them into a pandas dataframe
+train_path = tf.keras.utils.get_file(
+    "iris_training.csv", "https://storage.googleapis.com/download.tensorflow.org/data/iris_training.csv")
+test_path = tf.keras.utils.get_file("iris_test.csv", "https://storage.googleapis.com/download.tensorflow.org/data/iris_test.csv")
+
+train = pd.read_csv(train_path, names=CSV_COLUMN_NAMES, header=0)
+test = pd.read_csv(test_path, names=CSV_COLUMN_NAMES, header=0)
+
+train.head()
+train_y = train.pop('Species')
+test_y = test.pop('Species')
+train.head() # the species column in now gone
+train_y.head()
+train.shape()
+
+def input_fn(features, labels, training=True, batch_size=256):
+    # Convert the input into a Dataset
+    dataset = tf.data.Dataset.from_tensor_slices((dict(features), labels))
+
+    # Shuffle and repeat if you're in training mode.
+    if training:
+        dataset = dataset.shuffle(1000).repeat()
+
+    return dataset.batch(batch_size)
+
+# Feature columns describe how to use the input.
+feature_columns = []
+for key in train.keys():
+    feature_columns.append(tf.feature_column.numeric_column(key=key))
+print (feature_columns)
